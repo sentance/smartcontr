9f38e31a07bb4f742288183b4e2f7a0428509886
sentance <sentance22@gmail.com>
2021-05-14 15:56:55 +0300

    first drizle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import SetString from "./SetString";
+import ReadString from "./ReadString";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    const { drizzle } = this.props;
+  
+    // subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+  
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+  
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  state = { loading: true, drizzleState: null };
+  render() {
+    if (this.state.loading) return "Loading Drizzle...";
+    return (
+      <div className="App">
+        <ReadString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+        <SetString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
